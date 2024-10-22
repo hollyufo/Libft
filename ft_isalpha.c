ddcf5d85cb1c174cd0e571e405cb0b4fdcdeeee5
Imrane Chaibi <imchaibi@c4r7p6.1337.ma>
2024-10-22 11:57:46 +0100

    done with ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
-#include <stdio.h>
 
 int ft_isalpha(char c)
 {
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
     {
-        return 1;
+        return (1);
     }
-    return 0;
+    return (0);
 }
