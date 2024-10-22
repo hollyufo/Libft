3f83feb5179e256aafc5d0d06168930c81f186f9
Imrane Chaibi <imchaibi@c4r9p3.1337.ma>
2024-10-22 16:27:56 +0100

    done

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,12 @@
 /*   By: imchaibi <imchaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:36:09 by imchaibi          #+#    #+#             */
-/*   Updated: 2024/10/22 13:36:10 by imchaibi         ###   ########.fr       */
+/*   Updated: 2024/10/22 16:27:33 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 int ft_isalpha(int c)
 {
