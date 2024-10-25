62119a7843fc6c82030cc117d869d8af409070b8
Imrane Chaibi <imchaibi@c4r7p5.1337.ma>
2024-10-25 13:49:46 +0100

    done with strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imchaibi <imchaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 15:45:05 by imchaibi          #+#    #+#             */
-/*   Updated: 2024/10/25 13:46:18 by imchaibi         ###   ########.fr       */
+/*   Created: 2024/10/25 13:22:22 by imchaibi          #+#    #+#             */
+/*   Updated: 2024/10/25 13:47:00 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char *ft_strchr(const char *s, int c)
+#include <unistd.h>
+
+char *ft_strrchr(const char *s, int c)
 {
     int i;
     char char_to_find;
+    char result;
 
     char_to_find = (char) c;
     i = 0;
@@ -24,13 +27,13 @@ char *ft_strchr(const char *s, int c)
     {
         if(s[i] == char_to_find)
         {
-            return ((char *) &s[i]);
+            result = (char *) &s[i];
         }
         i++;
     }
     if (s[i] == char_to_find)
     {
-        return ((char *) &s[i])
+        result = (char *) &s[i];
     }
-    return (NULL);
+    return (result);
 }
