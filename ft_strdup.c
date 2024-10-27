d924cd6c534db4aaa612226724c7b159c5209779
Imrane Chaibi <imchaibi@c4r9p10.1337.ma>
2024-10-27 13:29:21 +0100

    optimized strdub

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: imchaibi <imchaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 12:50:18 by imchaibi          #+#    #+#             */
-/*   Updated: 2024/10/27 12:55:21 by imchaibi         ###   ########.fr       */
+/*   Updated: 2024/10/27 13:28:58 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ size_t ft_strlen(const char *str);
 
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 
+void *ft_calloc(size_t count, size_t size);
+
 char *ft_strdup(const char *src)
 {
     size_t len = ft_strlen(src);
-    char *dub = (char *)malloc((len + 1) * sizeof(char));
+    char *dub = (char *)ft_calloc(len + 1, sizeof(char));
 
     if (dub == NULL)
     {
