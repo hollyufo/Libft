470009935278dfc41b55458a0f0ac4506008556f
Imrane Chaibi <imchaibi@c4r10p10.1337.ma>
2024-10-28 17:53:24 +0100

    backup

---
+++ ft_strjoin.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imchaibi <imchaibi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 15:58:16 by imchaibi          #+#    #+#             */
+/*   Updated: 2024/10/27 20:44:03 by imchaibi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+size_t ft_strlen(const char *str);
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    size_t s1_len = s1 ? ft_strlen(s1) : 0;
+    size_t s2_len = s2 ? ft_strlen(s2) : 0;
+    
+    if (!s1 && !s2)
+    {
+        return (char *)ft_calloc(1, sizeof(char));
+    }
+
+    if (!s1)
+    {
+        char *result = (char *)ft_calloc(s2_len + 1, sizeof(char));
+        if (result)
+        {
+            ft_strlcpy(result, s2, s2_len + 1);
+        }
+        return result;
+    }
+
+    if (!s2)
+    {
+        char *result = (char *)ft_calloc(s1_len + 1, sizeof(char));
+        if (result)
+        {
+            ft_strlcpy(result, s1, s1_len + 1);
+        }
+        return result;
+    }
+
+    char *str_result = (char *)ft_calloc(s1_len + s2_len + 1, sizeof(char));
+    if (!str_result)
+    {
+        return NULL;
+    }
+
+    ft_strlcpy(str_result, s1, s1_len + 1);
+    ft_strlcpy(str_result + s1_len, s2, s2_len + 1);
+
+    return str_result;
+}
