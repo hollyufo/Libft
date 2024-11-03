44ebb949667ca3a36e94a54b79603c0a7ca784b3
hollyufo <imranchaibi1@gmail.com>
2024-11-04 00:13:15 +0100

    norminette part 4

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: imchaibi <imchaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:44:17 by imchaibi          #+#    #+#             */
-/*   Updated: 2024/11/03 18:35:16 by imchaibi         ###   ########.fr       */
+/*   Updated: 2024/11/03 23:59:27 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,17 @@ static int	count_words(const char *str, char delimiter)
 	int	count;
 	int	in_word;
 
-	in_word = 0;
 	count = 0;
+	in_word = 0;
 	while (*str)
 	{
-		if (*str != delimiter)
+		if (*str != delimiter && !in_word)
 		{
-			if (!in_word)
-			{
-				in_word = 1;
-				count++;
-			}
+			count++;
+			in_word = 1;
 		}
-		else
-		{
+		else if (*str == delimiter)
 			in_word = 0;
-		}
 		str++;
 	}
 	return (count);
@@ -58,48 +53,39 @@ static char	*allocate_word(const char *s, char c)
 	return (word);
 }
 
-char **ft_split(const char *s, char c)
+static char	**free_split(char **result, int i)
 {
-    if (!s)
-        return NULL;
+	while (i > 0)
+		free(result[--i]);
+	free(result);
+	return (NULL);
+}
 
-    int word_count = count_words(s, c);
-    
-    // Handle empty string and only delimiters case
-    if (word_count == 0)
-    {
-        char **empty_result = (char **)ft_calloc(2, sizeof(char *));
-        if (!empty_result)
-            return NULL;
-        empty_result[0] = ft_calloc(1, sizeof(char)); // Empty string entry
-        return empty_result;
-    }
+char	**ft_split(const char *s, char c)
+{
+	int		word_count;
+	char	**result;
+	int		i;
 
-    char **result = (char **)ft_calloc(word_count + 1, sizeof(char *)); // Null-terminated array
-    if (!result)
-        return NULL;
-
-    int i = 0;
-    while (*s)
-    {
-        if (*s != c)
-        {
-            result[i] = allocate_word(s, c);
-            if (!result[i])
-            {
-                while (i > 0) // Free previously allocated memory on failure
-                    free(result[--i]);
-                free(result);
-                return NULL;
-            }
-            i++;
-            while (*s && *s != c)
-                s++;
-        }
-        else
-        {
-            s++;
-        }
-    }
-    return result;
+	i = 0;
+	if (!s)
+		return (NULL);
+	word_count = count_words(s, c);
+	result = (char **)ft_calloc(word_count + 1, sizeof(char *));
+	if (!result)
+		return (NULL);
+	while (i < word_count)
+	{
+		while (*s == c)
+			s++;
+		result[i] = allocate_word(s, c);
+		if (!result[i])
+		{
+			return (free_split(result, i));
+		}
+		while (*s && *s != c)
+			s++;
+		i++;
+	}
+	return (result);
 }
